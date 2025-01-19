a2cf9f00d42f084b5c4c4fe478d80734bdc43d7f
Eldy Effendi <eldyeffendi9@gmail.com>
2025-01-20 05:01:53 +0700

    add sice string method to substr

--- 9-strings-methods.js
+++ 9-strings-methods.js
@@ -76,3 +76,51 @@ Itu membuat string terlihat seperti array (padahal sebenarnya tidak)
 Jika tidak ada karakter yang ditemukan, [ ] mengembalikan tidak terdefinisi, sedangkan charAt() mengembalikan string kosong.
 Itu hanya bisa dibaca. str[0] = "A" tidak memberikan kesalahan (tetapi tidak berfungsi!)
 */
+text5[0] = "A";
+
+//Extracting String Parts
+//Ada 3 metode untuk ekstrak part sebuah string:
+/*
+1.slice(start, end)
+2.substring(start, end)
+3.substr(start, length)
+*/
+
+//1.Slice
+//mengekstrak sebuah string menjadi string baru
+//metode itu menggunakan 2 paraeter yaitu start position, dan end position (end tidak termasuk)
+let texti = "Apple, Banana, Kiwi";
+let partSlice1 = texti.slice(7, 13);
+partSlice1;
+
+//Jika hanya menggunakan 1 parameter maka akan mengembalikan/memotong sisa String
+let partSlice2 = texti.slice(7);
+partSlice2;
+
+//Jika menggunakan angka negatif di parameter, maka akan memotong dari end atau dari string akhir ke awal
+let partSlice3 = texti.slice(-12);
+partSlice3
+
+let partSlice4 = texti.slice(-12, -6);
+partSlice4
+
+//2.substring
+//substring() mirip dengan slice().
+//Perbedaannya adalah nilai awal dan akhir kurang dari 0 diperlakukan sebagai 0 di substring().
+//Jadi ngga bisa parameter minus, seperti(-12, -6), hasilnya akan Apple, Banana, Kiwi
+let subs1 = texti.substring(-12, 5);
+subs1;
+
+//3.substr
+//substr() mirip dengan slice().
+//Perbedaannya adalah parameter kedua menentukan panjang bagian yang diekstrak.
+let substr1 = texti.substr(7,12);
+substr1;
+
+//Jika parameternya hanya satu maka akan mencetak akhir dari urutan string
+let substr2 = texti.substr(7);
+substr2;
+
+//Jika parameternya negatif maka akan dihitung mulai dari akhir string ke urutan akhir
+let substr3 = texti.substr(-4);
+substr3;
