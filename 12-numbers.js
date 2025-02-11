d3b092c7e8cc3499151e4a39a8b8954f58fd469e
Eldy Effendi <eldyeffendi9@gmail.com>
2025-02-11 14:16:34 +0700

    add progress number

---
+++ 12-numbers.js
@@ -0,0 +1,19 @@
+//Number bisa ditulis desimal dan tidak
+let x1 = 3.5;
+let x2 = 2;
+
+//Operator + hanya berfungsi untuk menambahkan number dan gabung string
+let x3 = 20;
+let x4 = "30";
+let x5 = x3 + x4;
+x5;
+
+let x6 = 10;
+let x7 = x3 + x6;
+x7;
+//Operasi ekspresi dimulai dari kiri ke kanan, jika di ada nomor bertemu string dengan operator (+) maka jadi menggabung
+//NaN adalah not a number yaitu memberitahu bahwa ada tipe nomor yang ilegal
+//Fungsi isNaN() untuk mencari jika sebuah nilai bukan nmber
+//Fungsi typeOf NaN(), mengembalikan nomor
+//Infinity (or -Infinity) adalah nilai javascript yang akan mengembalikan nilai jika menghitung angka sangat besar
+//Javascript Numbers as Objects, number bisa menjadi objek dengan keyword new
